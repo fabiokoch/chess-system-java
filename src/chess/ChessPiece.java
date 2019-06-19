6256bf1f75bdbb3e3b5a10bfec9fd852af2b129d
fabiokoch <fabiokochdaluz@hotmail.com>
2019-06-19 13:56:10 -0300

    Chek logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -16,10 +16,15 @@ public abstract class ChessPiece extends Piece {
 	public Color getColor() {
 		return color;
 	}	
+
 	
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
+	}
+	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
 	}
 	
 }
